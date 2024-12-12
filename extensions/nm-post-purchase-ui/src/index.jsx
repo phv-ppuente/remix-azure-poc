1230c15ea3aecf317f0fd1b6992d391bdb09e355
phv-ppuente <113539820+phv-ppuente@users.noreply.github.com>
2024-12-12 13:27:08 -0600

    update URL

--- extensions/nm-post-purchase-ui/src/index.jsx
+++ extensions/nm-post-purchase-ui/src/index.jsx
@@ -17,7 +17,7 @@ import {
 } from "@shopify/post-purchase-ui-extensions-react";
 
 // For local development, replace APP_URL with your local tunnel URL.
-const APP_URL = "https://nikon-possession-picks-reconstruction.trycloudflare.com";
+const APP_URL = "https://Remix-App-POC.azurewebsites.net";
 
 // Preload data from your app server to ensure that the extension loads quickly.
 extend(
